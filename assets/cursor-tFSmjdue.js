3ffee01f15e71b5eefa75a27edcc0c2abee08e49
Xeron2000 <Xeron2000@users.noreply.github.com>
2025-11-19 10:58:41 +0000

    deploy: 70ff5059e4bec34bdd35b0be4bd801d116b12567

---
+++ assets/cursor-tFSmjdue.js
@@ -0,0 +1,3 @@
+import{b as o}from"./core-DB65027A.js";import"./index-fpAAkCtu.js";import"./events-CVmz8IQ4.js";import"./index.es-CmeVAQIs.js";import"./fallback-BsJoIGXi.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
+  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
+</svg>`;export{l as cursorSvg};
